7d5001e4462f082a9e5815511b536ba1ed33ed98
Mukesh Negi <mukeshsingh.negi07@gmail.com>
2025-08-16 12:46:52 +0000

    To get your site ready to show ads, copy and paste this HTML <meta> tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9187440931404634" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
