b2fd2622b6fadcba7b66f46cd9371a3dcb6f4993
Mukesh Negi <mukeshsingh.negi07@gmail.com>
2025-10-10 08:53:45 +0000

    google ads pushed

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,6 @@ import AdSenseUnit from '../AdSenseUnit';
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <AdSenseUnit adSlot="5342210952" autoMode='auto' />
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <LogoIcon className="h-6 w-6 text-primary" />
@@ -21,6 +20,7 @@ export function Header() {
             </Suspense>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
+        <AdSenseUnit adSlot="5342210952" autoMode='auto' />
           <Suspense fallback={<div />}>
             <SearchInput />
           </Suspense>
